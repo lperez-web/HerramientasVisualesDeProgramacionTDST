7028ed310868a3bcfa525a1be9f3f8bda4d0ebef
lperez-web <l.perez@unimayor.edu.co>
2025-08-28 14:44:36 -0500

    Clase importante

--- 01-bases-react/src/components/BasicTypes.tsx
+++ 01-bases-react/src/components/BasicTypes.tsx
@@ -1,22 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export const BasicTypes = () => {
-  const name: string = "vane";
-  const age: number = 18;
-   let isActive: boolean = true;
+  //let name: string = "vane"; // variable normal
+   const [name, setName] = useState<string>("vane");
+  const [age, setAge] = useState<number>(18);
+  const [isActive,setIsActive] = useState<boolean>(true);
+  //let isActive: boolean = true;
 
   return (
     <>
       <h3>Basic Types</h3>
-      <ul>
-        <li>Nombre: {name}</li>
-        <li>Edad: {age}</li>
-      </ul>
-      <li>Nombre: {age}</li>
-        <li>Edad: {isActive}</li>
-    </>
-)
+      {isActive ? (
+        <ul>
+          <li>Nombre: {name}</li>
+          <li>Edad: {age}</li>
+        </ul>
+      ) : (
+        <p>Usuario inactivo</p>
+      )}
 
-}
+      <h2>Variable normal</h2>
+      <button onClick={() => {
+          setName  ("lady");
+          console.log("Nuevo nombre:", name);
+        }}
+      >
+        Modificar nombre
+      </button>
+
+      <h2>Variable de estado age</h2>
+      <button
+        onClick={() => {
+          setAge(age + 1);
+          console.log("Nueva edad:", age + 1);
+        }}
+      >
+        Modificar edad
+      </button>
+
+      <h2>Estado de actividad</h2>
+      <button onClick={() => setIsActive(!isActive)}>
+        {isActive ? 'Desactivar usuario' : 'Activar usuario'}
+      </button>
+    </>
+  );
+};
+
+
 
 
