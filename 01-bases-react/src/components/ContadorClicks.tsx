1b3d9fe5d9d30dff8610cb1f6bdd666629ea7f54
lperez-web <l.perez@unimayor.edu.co>
2025-09-09 18:18:46 -0500

    listas

--- 01-bases-react/src/components/ContadorClicks.tsx
+++ 01-bases-react/src/components/ContadorClicks.tsx
@@ -1,6 +1,12 @@
+// imporatmos el react los useState y useEffect desde la libreria 
 import React, { useState, useEffect } from "react";
 
+//Componente funcional llamado contadorclicks 
 export const ContadorClicks = () => {
+  //DEfinimos el estado count con unvalor inicial en 0
+  // useState devuelve dos cosas:
+  //  count  el valor actual del estado
+  //  setCount función para actualizar el valor del estado
   // Estado inicial del contador
   const [count, setCount] = useState<number>(0);
 
@@ -10,14 +16,14 @@ export const ContadorClicks = () => {
   }, [count]);
 
   return (
+     // al hacer clic incrementa en 1
     <>
       <h3>Contador de Clics</h3>
       <p>Has hecho clic <strong>{count}</strong> veces</p>
-
+      
       <button onClick={() => setCount(count + 1)}>
         Hacer clic
       </button>
     </>
   );
 };
-
--- 01-bases-react/src/components/ProductoDetalles.tsx
+++ 01-bases-react/src/components/ProductoDetalles.tsx
@@ -1,26 +1,31 @@
 import React, { useState, useEffect } from "react";
-
+// componente funcional 
 export const ProductoDetalles = () => {
-  // Estado inicial para mostrar/ocultar detalles
-  const [showDetails, setShowDetails] = useState<boolean>(false);
 
-  // useEffect que se ejecuta cada vez que cambia "showDetails"
+  //controla si se muestra o no los detalles del producto 
+  //Estado inicial para mostrar/ocultar detalles
+  const [showDetails, setShowDetails] = useState(false);
+
+  //useEffect ejecuta cada vez que cambia "showDetails"
   useEffect(() => {
     if (showDetails) {
+      // si es trur muestra los detalles 
       console.log("Detalles del producto mostrados");
     } else {
+      // si es false no los muestra 
       console.log("Detalles del producto ocultos");
     }
-  }, [showDetails]);
+  }, [showDetails]);// este depende de showdetails 
 
   return (
+    //es una condicion  showDetails && 
     <>
       <h3>Producto en Tienda</h3>
       
       <button onClick={() => setShowDetails(!showDetails)}>
         {showDetails ? "Ocultar detalles" : "Mostrar detalles"}
       </button>
-
+       
       {showDetails && (
         <ul>
           <li><strong>Nombre:</strong> Bicicleta </li>
